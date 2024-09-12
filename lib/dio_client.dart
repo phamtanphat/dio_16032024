cea2893efc795f8cb515e33e96b5ed869ce73f72
phatdroid94 <phatdroid94@gmail.com>
2024-09-12 19:03:40 +0700

    Setup dio client

--- lib/dio_client.dart
+++ lib/dio_client.dart
@@ -1,37 +1,45 @@
 import 'package:dio/dio.dart';
 
 class DioClient {
-  // Tao ra function viec khoi tao dio
-  // Tao ra function khoi tao request
-
   static Dio? _dio;
 
   static Dio getDio() {
-    return _dio ??= Dio();
+    return _dio ??= _createDio();
+  }
+
+  static Dio _createDio() {
+    var dio = Dio(BaseOptions(
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      sendTimeout: const Duration(seconds: 10),
+      baseUrl: "https://jsonplaceholder.typicode.com",
+    ));
+
+    dio.interceptors.add(LogInterceptor());
+    return dio;
   }
 }
 
-class Person {
-  final String name;
-
-  const Person(this.name);
-
-  @override
-  bool operator == (Object other) =>
-      identical(this, other) ||
-          other is Person &&
-              runtimeType == other.runtimeType &&
-              name == other.name;
-
-  @override
-  int get hashCode => name.hashCode;
-}
-
-void main() {
-  Person person1 = Person("phat1");
-
-  Person person2 = Person("phat");
-
-  print(person1.hashCode);
-  print(person2.hashCode);
-}
+// Demo so sanh cac object voi nhau
+// class Person {
+//   final String name;
+//
+//   const Person(this.name);
+//
+//   @override
+//   bool operator ==(Object other) =>
+//       identical(this, other) ||
+//       other is Person && runtimeType == other.runtimeType && name == other.name;
+//
+//   @override
+//   int get hashCode => name.hashCode;
+// }
+//
+// void main() {
+//   Person person1 = Person("phat1");
+//
+//   Person person2 = Person("phat");
+//
+//   print(person1.hashCode);
+//   print(person2.hashCode);
+// }
