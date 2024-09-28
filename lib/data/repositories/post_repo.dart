a2a02ecc236e9a3653d7abddf9229f1589c66094
phatdroid94 <phatdroid94@gmail.com>
2024-09-28 18:07:40 +0700

    Refactor code

--- lib/data/repositories/post_repo.dart
+++ lib/data/repositories/post_repo.dart
@@ -1,23 +1,19 @@
-import 'package:dio/dio.dart';
+import 'dart:async';
 import 'package:dio_16032024/data/api/api_service.dart';
 import 'package:dio_16032024/data/api/dto/post_dto.dart';
 
 class PostRepository {
   final ApiService _service = ApiService();
 
-  void getPosts() {
-    _service.getPosts()
-        .then((response) {
-          List<PostDTO> listPostDTO = (response.data as List).map((element) => PostDTO.fromJson(element)).toList();
-          print(listPostDTO);
-        })
-        .catchError((error) => print("Error ${error.toString()}"));
+  Future<List<PostDTO>> getPosts() {
+    Completer<List<PostDTO>> completer = Completer();
+    _service.getPosts().then((response) {
+      List<PostDTO> listPostDTO = (response.data as List)
+          .map((element) => PostDTO.fromJson(element))
+          .toList();
+      completer.complete(listPostDTO);
+    }).catchError((error) => completer.completeError(error));
+
+    return completer.future;
   }
 }
-
-
-void main() {
-  var repo = PostRepository();
-  repo.getPosts();
-}
-
